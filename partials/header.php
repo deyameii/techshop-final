7eb4ef8ba1ca7e99837e236a110cdfd94fd60a2b
deyameii <behamoth91@icloud.com>
2022-10-04 14:17:08 +0300

    animations

--- partials/header.php
+++ partials/header.php
@@ -6,15 +6,15 @@
             <li><a href="#"><i class="fa-solid fa-map-location data-scroll mr-2"></i>ул. Грига 5</a></li>
         </ul>
         <ul class="text-white flex gap-6">
-            <?php if (isset($_SESSION['error'])): ?>
+            <?php if (isset($_SESSION['error'])) : ?>
                 <li class="error text-red-300"><?php echo $_SESSION['error']; ?></li>
                 <?php unset($_SESSION['error']); ?>
             <?php endif; ?>
 
-            <?php if (isset($_SESSION['user'])): ?>
+            <?php if (isset($_SESSION['user'])) : ?>
                 <li><?php echo $_SESSION['user']['email']; ?></li>
                 <a href="src/php/logout.php">Выйти</a>
-            <?php else: ?>
+            <?php else : ?>
                 <li><a href="#" id="login"><i class="fa-solid fa-right-to-bracket mr-2"></i>Личный кабинет</a></li>
             <?php endif; ?>
         </ul>
@@ -24,10 +24,11 @@
 <!-- header-->
 <header class="bg-[#111827] h-24 flex items-center ">
     <div class="container my-0 mx-auto py-0 px-4 max-w-7xl flex items-center gap-28 justify-between">
-        <img src="src/resources/img/logo.png" alt="logo" class="mt-1.5">
+        <a href="../index.php" class="">
+            <img src="src/resources/img/logo.png" alt="logo" class="mt-1.5">
+        </a>
         <div class="relative flex flex-col items-center gap-2 text-white">
-            <div
-                class="absolute right-0.5 top-[-10px] w-5 h-5 leading-5 text-center rounded-full text-xs text-white bg-[#9591e1]">
+            <div class="absolute right-0.5 top-[-10px] w-5 h-5 leading-5 text-center rounded-full text-xs text-white bg-[#9591e1]">
                 2
             </div>
             <i class="fa-solid fa-basket-shopping"></i>
@@ -39,7 +40,7 @@
 <nav class="bg-main font-medium text-white">
     <div class="container my-0 mx-auto py-4 px-4 max-w-7xl flex gap-28 justify-center">
         <ul class="flex gap-24 items-center">
-            <li><a href="" class="hover:text-black">Главная</a></li>
+            <li><a href="../index.php" class="hover:text-black">Главная</a></li>
             <li><a href="../catalog.php" class="hover:text-black">Каталог</a></li>
             <li><a href="" class="hover:text-black">О нас</a></li>
             <li><a href="" class="hover:text-black">Отзывы</a></li>
